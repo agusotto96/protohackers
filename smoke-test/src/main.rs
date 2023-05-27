4769e581df547787c73b87f114575aec3b137002
aottolini <aottolini@agro.uba.ar>
2023-05-27 20:56:23 -0300

    simplify

--- smoke-test/src/main.rs
+++ smoke-test/src/main.rs
@@ -1,4 +1,3 @@
-use std::env::args;
 use std::io;
 use std::io::Read;
 use std::io::Write;
@@ -7,23 +6,22 @@ use std::net::TcpStream;
 use std::thread::spawn;
 
 fn main() -> io::Result<()> {
-    let address = args().nth(1).unwrap_or("0.0.0.0:8080".into());
-    let listener = TcpListener::bind(address)?;
-    for stream in listener.incoming() {
+    let listener = TcpListener::bind("0.0.0.0:8080")?;
+    loop {
+        let (stream, _) = listener.accept()?;
         spawn(|| {
-            let _ = stream.and_then(process_stream);
+            let _ = process_stream(stream);
         });
     }
-    Ok(())
 }
 
 fn process_stream(mut stream: TcpStream) -> io::Result<()> {
     loop {
-        let mut buf = [0; 1028];
-        let n = stream.read(&mut buf)?;
+        let mut buffer = [0; 1028];
+        let n = stream.read(&mut buffer)?;
         if n == 0 {
             return Ok(());
         }
-        stream.write_all(&buf[0..n])?
+        stream.write_all(&buffer[0..n])?
     }
 }
