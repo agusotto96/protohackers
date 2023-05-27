34de74f49ca003b6f5ca15740c68c55019c5b6a8
aottolini <aottolini@agro.uba.ar>
2023-05-27 18:09:23 -0300

    remove tokio

--- smoke-test/src/main.rs
+++ smoke-test/src/main.rs
@@ -1,32 +1,29 @@
-use std::env;
-use tokio::io;
-use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
-use tokio::io::BufReader;
-use tokio::net::TcpListener;
-use tokio::net::TcpStream;
+use std::env::args;
+use std::io;
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread::spawn;
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let address = env::args().nth(1).unwrap_or("0.0.0.0:8080".into());
-    let listener = TcpListener::bind(address).await?;
-    loop {
-        let (socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            let _ = process_socket(socket).await;
+fn main() -> io::Result<()> {
+    let address = args().nth(1).unwrap_or("0.0.0.0:8080".into());
+    let listener = TcpListener::bind(address)?;
+    for stream in listener.incoming() {
+        spawn(|| {
+            let _ = stream.and_then(process_stream);
         });
     }
+    Ok(())
 }
 
-async fn process_socket(mut socket: TcpStream) -> io::Result<()> {
-    let (r_socket, mut w_socket) = socket.split();
-    let mut reader = BufReader::new(r_socket);
+fn process_stream(mut stream: TcpStream) -> io::Result<()> {
     loop {
-        let mut input = Vec::new();
-        let n = reader.read_buf(&mut input).await?;
+        let mut buf = [0; 1028];
+        let n = stream.read(&mut buf)?;
         if n == 0 {
             return Ok(());
         }
-        w_socket.write_all(&input).await?;
+        stream.write_all(&buf[0..n])?
     }
 }
