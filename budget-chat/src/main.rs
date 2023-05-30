879d656c209417e32ee420bde516225a7f0d8667
aottolini <aottolini@agro.uba.ar>
2023-05-30 06:51:38 -0300

    asd

--- budget-chat/src/main.rs
+++ budget-chat/src/main.rs
@@ -162,6 +162,7 @@ fn build_log_out_msg(name: &str) -> Message {
 
 fn parse_chat_msg(bytes: Vec<u8>, name: &str) -> Option<Message> {
     let value = String::from_utf8(bytes).ok()?;
+    let value = value.replace('\r', "");
     if value.is_empty() {
         return None;
     }
@@ -177,6 +178,7 @@ fn parse_chat_msg(bytes: Vec<u8>, name: &str) -> Option<Message> {
 
 fn parse_name(bytes: Vec<u8>) -> Option<String> {
     let name = String::from_utf8(bytes).ok()?;
+    let name = name.replace('\r', "");
     if name.is_empty() {
         return None;
     }
