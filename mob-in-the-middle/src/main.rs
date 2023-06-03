b26fd040d6777e42c0ed62e7bf87732548c87131
aottolini <aottolini@agro.uba.ar>
2023-06-03 16:39:20 -0300

    more rf

--- mob-in-the-middle/src/main.rs
+++ mob-in-the-middle/src/main.rs
@@ -1,17 +1,20 @@
+use std::env::args;
 use std::io;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Write;
+use std::net::Shutdown::Both;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread::spawn;
 
 const CHAT_ADDRESS: &str = "chat.protohackers.com:16963";
 
-const TONY_BOGUSCOIN_ADRRESS: &[u8; 27] = b"7YWHMfk9JZe0LM0g1ZauHuiSxhI";
+const TONY_BOGUSCOIN_ADRRESS: &str = "7YWHMfk9JZe0LM0g1ZauHuiSxhI";
 
 fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8080")?;
+    let address = args().nth(1).unwrap_or("0.0.0.0:8080".into());
+    let listener = TcpListener::bind(address)?;
     for stream in listener.incoming().flatten() {
         spawn(|| {
             let _ = handle_connection(stream);
@@ -37,41 +40,41 @@ fn intercept_msg(mut reader: BufReader<TcpStream>, mut stream: TcpStream) -> io:
     loop {
         let Ok(Some(msg)) = read_msg(&mut reader) else { break };
         let msg = tamper_msg(&msg);
-        let Ok(()) = stream.write_all(&msg) else { break };
+        let Ok(()) = write_msg(&mut stream, &msg) else { break };
     }
-    stream.shutdown(std::net::Shutdown::Both)?;
+    stream.shutdown(Both)?;
     Ok(())
 }
 
-fn read_msg(reader: &mut BufReader<TcpStream>) -> io::Result<Option<Vec<u8>>> {
-    let mut msg = Vec::new();
-    let n = reader.read_until(b'\n', &mut msg)?;
+fn read_msg(reader: &mut BufReader<TcpStream>) -> io::Result<Option<String>> {
+    let mut msg = String::new();
+    let n = reader.read_line(&mut msg)?;
     if n == 0 {
         return Ok(None);
     }
     Ok(Some(msg))
 }
 
-fn tamper_msg(msg: &[u8]) -> Vec<u8> {
-    let mut msg = msg.to_vec();
-    msg.pop();
-    let mut msg = msg
-        .split(|b| *b == b' ')
-        .map(|w| {
-            if is_boguscoin_address(w) {
-                TONY_BOGUSCOIN_ADRRESS.to_vec()
-            } else {
-                w.to_owned()
-            }
-        })
-        .collect::<Vec<Vec<u8>>>()
-        .join(&b' ');
-    msg.push(b'\n');
-    msg
+fn write_msg(stream: &mut TcpStream, msg: &str) -> io::Result<()> {
+    stream.write_all(msg.as_bytes())
 }
 
-fn is_boguscoin_address(word: &[u8]) -> bool {
-    word.starts_with(b"7")
+fn tamper_msg(msg: &str) -> String {
+    let msg = &msg[..msg.len() - 1];
+    msg.split_ascii_whitespace()
+        .map(|w| {
+            if is_boguscoin_address(w) {
+                TONY_BOGUSCOIN_ADRRESS
+            } else {
+                w
+            }
+        })
+        .collect::<Vec<&str>>()
+        .join(" ")
+}
+
+fn is_boguscoin_address(word: &str) -> bool {
+    word.starts_with('7')
         && (word.len() >= 26 && word.len() <= 35)
-        && word.iter().all(u8::is_ascii_alphanumeric)
+        && word.chars().all(char::is_alphanumeric)
 }
